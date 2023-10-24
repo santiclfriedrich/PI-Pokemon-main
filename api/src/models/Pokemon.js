e5028ccabd3513398d955b1421b30fba6cf34d28
santiclfriedrich <santiclear1@hotmail.com>
2023-10-24 16:19:52 -0300

    Subida a GitHub || DB terminado

---
+++ api/src/models/Pokemon.js
@@ -0,0 +1,43 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      default: DataTypes.UUIDV4
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    life: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    attack: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    defense: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    speed: {
+      type: DataTypes.STRING,
+    },
+    height: {
+      type: DataTypes.STRING,
+    },
+    weight: {
+      type: DataTypes.STRING
+    }
+  }, {timestamps: false});
+};
